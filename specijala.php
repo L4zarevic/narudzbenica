e622cb6292fbd8456074474c196566af4e584d3d
Nemanja-MO <lazarevic.nemanja@gmail.com>
2020-08-23 11:13:53 +0200

    Add user font awesome icon

--- specijala.php
+++ specijala.php
@@ -102,7 +102,7 @@ include '../narudzbenica/modules/header.php';
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Ulogovani ste kao <b><?php echo $imeKorisnika; ?></b></span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Ulogovani ste kao <b><?php echo $imeKorisnika; ?></b>  <i class="fas fa-user"></i></span>
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
