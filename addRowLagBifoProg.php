b5c7bb81bf23da2171a658d308503aa131151614
Nemanja-MO <lazarevic.nemanja@gmail.com>
2021-02-27 11:00:41 +0100

    Add missing column in SQL query

--- addRowLagBifoProg.php
+++ addRowLagBifoProg.php
@@ -16,22 +16,24 @@ $conn = OpenStoreCon($dataBaseName);
 mysqli_set_charset($conn, 'utf8');
 
 $stavka = mysqli_real_escape_string($conn, $_REQUEST['stavka']);
-$arS = explode("###", $stavka, 7);
+$arS = explode("###", $stavka, 8);
 $arS[1] = rtrim($arS[1], "###");
 
 $odOsOu = $arS[0];
 $vrstaSociva = $arS[1];
-$sph = $arS[2];
-$add = $arS[3];
-$jm = $arS[4];
-$kolicina = $arS[5];
-$napomena = $arS[6];
+$materijal = $arS[2];
+$sph = $arS[3];
+$add = $arS[4];
+$jm = $arS[5];
+$kolicina = $arS[6];
+$napomena = $arS[7];
 
-$upit = "insert into narudzbenica (IDKorisnika,lag_spec,od_os_ou,vrsta_sociva,sph,adicija,jm,kolicina,napomena) values ('$idKorisnika','Lager','$odOsOu', '$vrstaSociva','$sph','$add','$jm','$kolicina','$napomena')";
-
-$rezultat = mysqli_query($conn, $upit);
+$stmt = $conn->prepare('INSERT INTO narudzbenica (IDKorisnika,lag_spec,od_os_ou,vrsta_sociva,vrsta_materijala,sph,adicija,jm,kolicina,napomena) values (?,"Lager",?,?,?,?,?,?,?,?)');
+$stmt->bind_param('issssssis', $idKorisnika, $odOsOu, $vrstaSociva, $materijal, $sph, $add, $jm, $kolicina, $napomena);
+$stmt->execute();
 if (mysqli_error($conn)) {
 	die(mysqli_error($conn));
 }
+
 header('Location: ../narudzbenica/lager_bifocal_progresiv.php');
 CloseCon($conn);
