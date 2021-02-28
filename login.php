10acf3b2e3ebd5835b9ff48d94ffe95d3e8c2104
Nemanja-MO <lazarevic.nemanja@gmail.com>
2021-02-28 18:21:44 +0100

    HTML minify

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>M-Optic</title>
+    <title>Moja Optika Stanković | e-Narudžbenica</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="noindex">
