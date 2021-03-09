b482ce6f1a556c8a7d5afcbed1430d33429c9e3b
Nemanja-MO <lazarevic.nemanja@gmail.com>
2021-03-09 12:30:48 +0100

    Edit footer

--- index.php
+++ index.php
@@ -54,8 +54,7 @@ mysqli_set_charset($conn, 'utf8');
                             <p>Sve željene stavke koje potvrdite nakon unosa biće prikazane u tabeli NARUDŽBENICA koja se nalazi na dnu stranice.</p>
                             <p><strong>Unesene količine za stavke u tabeli NARUDŽBENICA nije moguće uređivati.</strong></p>
                             <p>U slučaju pogrešnog unosa, stavku iz tabele je potrebno ukloniti klikom na ikonicu kantice <i class='fas fa-trash'></i>, a nakon toga izvršiti ponovni unos iz grafikona.</p>
-                            <p>Nakon što kreirate narudžbenicu, potrebno je kliknuti na dugme <i class='fas fa-paper-plane'></i> POŠALJI NARUDŽBU, nakog čega će Vaša narudžbenica biti poslata veleprodaji M-OPTIC</p>
-                            <p>Za sve dodatne informacije u vezi aplikacije, pišite na email: <a href="mailto:nemanja.lazarevic@mojaoptika.com">nemanja.lazarevic@mojaoptika.com</a></p></br>
+                            <p>Nakon što kreirate narudžbenicu, potrebno je kliknuti na dugme <i class='fas fa-paper-plane'></i> POŠALJI NARUDŽBU, nakog čega će Vaša narudžbenica biti poslata veleprodaji "M-OPTIC"</p>
                         </div>
                         <div class="companyInfo"> <img id="logo" src="../narudzbenica/images/MO.png">
                             <p>“M-OPTIC” d.o.o.</br> ul. Majevička br. 29, 76300 Bijeljina</br> <strong>Tel:</strong> +387 55 222 999, 222 990, 490 010</br> <strong>Fax:</strong> +387 55 222 998</br> <strong>Email:</strong> <a href="mailto:mopticvp@mojaoptika.com">mopticvp@mojaoptika.com</a></br> <a href="https://mojaoptika.com">www.mojaoptika.com</a></p>
--- modules/footer.php
+++ modules/footer.php
@@ -1,21 +1,22 @@
 <?php echo "<footer class='sticky-footer bg-white'>";
 echo "<div class='container my-auto'>";
 echo "<div class='copyright text-center my-auto'>";
-echo "<span>Copyright &copy; M-Optic " . date("Y") . "</span>";
+echo "<span>Copyright &copy; Moja Optika Stanković " . date("Y") . "</span></br>";
+echo "<span>Za sve probleme u vezi aplikacije, pišite na email: <a href='mailto:nemanja.lazarevic@mojaoptika.com'>nemanja.lazarevic@mojaoptika.com</a></span>";
 echo "</div>";
 echo "</div>";
 echo "</footer>"; ?></div>
 </div><a class="scroll-to-top rounded" href="#page-top"> <i class="fas fa-angle-up"></i></a>
 <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Odjava</h5> <button class="close" type="button" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">×</span> </button>
-            </div>
-            <div class="modal-body">Izaberite "Odjava" da bi završili trenutnu sesiju.</div>
-            <div class="modal-footer"> <button class="btn btn-secondary" type="button" data-dismiss="modal">Prekini</button> <a class="btn btn-primary" href="../narudzbenica/logout.php">Odjava</a> </div>
-        </div>
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Odjava</h5> <button class="close" type="button" data-dismiss="modal" aria-label="Close"> <span aria-hidden="true">×</span> </button>
+      </div>
+      <div class="modal-body">Izaberite "Odjava" da bi završili trenutnu sesiju.</div>
+      <div class="modal-footer"> <button class="btn btn-secondary" type="button" data-dismiss="modal">Prekini</button> <a class="btn btn-primary" href="../narudzbenica/logout.php">Odjava</a> </div>
     </div>
+  </div>
 </div>
 <script src="vendor/jquery/jquery.min.js"></script>
 <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
