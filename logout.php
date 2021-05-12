5259a715a13238d068abba57355e3052437d3c2b
Nemanja-MO <lazarevic.nemanja@gmail.com>
2021-05-12 09:07:14 +0200

    Edit session end

--- logout.php
+++ logout.php
@@ -1,1 +1,1 @@
-<?php session_start();$_SESSION = array();die(header('Location: ../narudzbenica/login.php'));?>
+<?php session_start();unset($_SESSION["login"]);die(header('Location: ../narudzbenica/login.php'));?>
--- moptic/thanks.php
+++ moptic/thanks.php
@@ -1,21 +1,3 @@
-<!DOCTYPE html>
-<html lang="en"><?php include '../modules/header.php'; ?>
-
-<body id="page-top">
-    <div id="wrapper">
-        <?php include 'modules/menu.php'; ?>
-        <div id="content-wrapper" class="d-flex flex-column">
-            <div id="content">
-                <?php
-                include '../modules/logout.php';
-                ?>
-                <div class="container-fluid">
-                    <div class="thankYou">
-                        <h2 style="color:#0fad00">Narudžbina uspješna</h2> </br> <img id="checkMark" src="../images/green-check-icon.png"> </br></br>
-                        <p style="font-size:20px;color:#5C5C5C;">Vaša narudžbina je uspješno poslata na email: <a href="mailto:narudzba@mojaoptika.com">narudzba@mojaoptika.com</a></p>&nbsp;&nbsp;&nbsp;&nbsp;<a id="homeButton" href="../index.php" class="btn btn-success"> Idi na početnu </a> <br><br>
-                    </div>
-                </div>
-            </div><?php include '../modules/footer.php'; ?>
-</body>
-
-</html>
+<?php
+include '../thanks.php';
+?>
