66813f61a353de7ae16245fc671ce1bf51170048
Nemanja-MO <lazarevic.nemanja@gmail.com>
2021-05-15 18:34:25 +0200

    Edit manual text

--- essilor/index.php
+++ essilor/index.php
@@ -30,13 +30,14 @@ mysqli_set_charset($conn, 'utf8');
                         <h1 class="h3 mb-0 text-gray-800">e-Narudžbenica &nbsp;<i class="fa fa-clipboard-list"></i></h1>
                     </div>
                     <div class="row">
-                        <div class="">
+                        <div class="form-group col-md-12">
                             <h5>Uputstvo za narudžbenicu</h5>
                             <p>Da biste kreirali željenu narudžbenicu, potrebno je u meniju lijevo izabrati Narudžba</p>
                             <p>Sve željene stavke koje potvrdite nakon unosa biće prikazane u tabeli NARUDŽBENICA koja se nalazi na dnu stranice.</p>
-                            <p>U slučaju pogrešnog unosa, stavku iz tabele je potrebno ukloniti klikom na ikonicu kantice <i class='fa fa-trash'></i>, a nakon toga izvršiti ponovni unos iz grafikona.</p>
-                            <p>Nakon što kreirate narudžbenicu, potrebno je kliknuti na dugme <i class='fa fa-paper-plane'></i> POŠALJI NARUDŽBU, nakog čega će Vaša narudžbenica biti poslata veleprodaji "M-OPTIC"</p>
+                            <p>U slučaju pogrešnog unosa, stavku iz tabele je moguće ukloniti klikom na ikonicu kantice <i class='fa fa-trash'></i>.</p>
+                            <p>Kada kreirate narudžbenicu, potrebno je kliknuti na dugme <i class='fa fa-paper-plane'></i> POŠALJI NARUDŽBU, nakog čega će Vaša narudžbenica biti poslata veleprodaji "M-OPTIC"-a</p>
                         </div>
+                        </br>
                         <div class="companyInfo"> <img id="logo" src="../images/essilor.png">
                             <!-- <p>“M-OPTIC” d.o.o.</br> ul. Majevička br. 29, 76300 Bijeljina</br> <strong>Tel:</strong> +387 55 222 999, 222 990, 490 010</br> <strong>Fax:</strong> +387 55 222 998</br> <strong>Email:</strong> <a href="mailto:mopticvp@mojaoptika.com">mopticvp@mojaoptika.com</a></br> <a href="https://mojaoptika.com">www.mojaoptika.com</a></p>-->
                         </div>
--- poloptic/index.php
+++ poloptic/index.php
@@ -31,17 +31,18 @@ mysqli_set_charset($conn, 'utf8');
                         <h1 class="h3 mb-0 text-gray-800">e-Narudžbenica &nbsp;<i class="fa fa-clipboard-list"></i></h1>
                     </div>
                     <div class="row">
-                        <div class="">
+                        <div class="form-group col-md-12">
                             <h5>Uputstvo za narudžbenicu</h5>
                             <p>Da biste kreirali željenu narudžbenicu, potrebno je u meniju lijevo izabrati iz ponude Lagerska stakla ili Specijala</p>
                             <p><strong> Lagerska stakla:</strong></p>
-                            <p>Iz padajuće liste izaberite željeni index. U polje grafikona unijeti željenu količinu dioptrije, nakon čega je potrebno pritisnuti ENTER na tastaturi radi potvrde.</p>
+                            <p>Iz padajuće liste izaberite željeni index. U polje grafikona unijeti željenu količinu dioptrije, nakon čega je potrebno pritisnuti ENTER na tastaturi radi potvrde unosa.</p>
                             <p><strong>NAPOMENA: Stavke dodavati jednu po jednu</strong></p>
                             <p>Sve željene stavke koje potvrdite nakon unosa biće prikazane u tabeli NARUDŽBENICA koja se nalazi na dnu stranice.</p>
                             <p><strong>Unesene količine za stavke u tabeli NARUDŽBENICA nije moguće uređivati.</strong></p>
-                            <p>U slučaju pogrešnog unosa, stavku iz tabele je potrebno ukloniti klikom na ikonicu kantice <i class='fa fa-trash'></i>, a nakon toga izvršiti ponovni unos iz grafikona.</p>
-                            <p>Nakon što kreirate narudžbenicu, potrebno je kliknuti na dugme <i class='fa fa-paper-plane'></i> POŠALJI NARUDŽBU, nakog čega će Vaša narudžbenica biti poslata veleprodaji "M-OPTIC"</p>
+                            <p>U slučaju pogrešnog unosa, stavku iz tabele možete ukloniti klikom na ikonicu kantice <i class='fa fa-trash'></i>, a nakon toga izvršiti ponovni unos iz grafikona.</p>
+                            <p>Kada kreirate narudžbenicu, potrebno je kliknuti na dugme <i class='fa fa-paper-plane'></i> POŠALJI NARUDŽBU, nakog čega će Vaša narudžbenica biti poslata veleprodaji "M-OPTIC"-a</p>
                         </div>
+                        </br>
                         <div class="companyInfo"> <img id="logo" src="../images/poloptic.png">
                             <!-- <p>“M-OPTIC” d.o.o.</br> ul. Majevička br. 29, 76300 Bijeljina</br> <strong>Tel:</strong> +387 55 222 999, 222 990, 490 010</br> <strong>Fax:</strong> +387 55 222 998</br> <strong>Email:</strong> <a href="mailto:mopticvp@mojaoptika.com">mopticvp@mojaoptika.com</a></br> <a href="https://mojaoptika.com">www.mojaoptika.com</a></p>-->
                         </div>
